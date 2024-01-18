b81ea373592c49e5d110bf75d9f3d7c21091543c
mark-use-blender <markyoutub00@gmail.com>
2024-01-18 15:10:38 +0800

    cleanup code

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-//openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program pico-tail-controller.elf verify reset exit"
-
+/*
+openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program pico-tail-controller.elf verify reset exit"
+*/
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,12 +19,8 @@
 uint8_t in_buf0[BUF_LEN];
 uint8_t chin_buf0[BUF_LEN];
 uint8_t sin_buf0[SBUF_LEN];
-char ddidh;
-char ddidl;
 uint8_t ddid[2];
 uint8_t in_bufid[2];
-
-//ddid[0] = '0';
 uint16_t adcinr0;
 uint16_t adcinr1;
 uint16_t temadcinr0;
@@ -44,15 +41,10 @@ int cmdel;
 char scmdel[13];
 char uin;
 int suin;
-
 char* sresult;
-//uint16_t result = adc_read();
 char int_str[50];
 float fresult;
 bool in_ok;
-
-
-
 
 int posdiff(int st,int nd ){
     int delta = nd-st;
@@ -67,78 +59,56 @@ int posdiff(int st,int nd ){
         }
     }
     return delta;
-        
 }
-
 void core1(){
     while(true){
-        
-        
-        
         adc_select_input(0);
         temadcinr0 = adc_read();
         curpos0 = curpos0+posdiff(adcinr0,temadcinr0);
         adcinr0 = temadcinr0;
         difpos0 =tarpos0-curpos0;
         if (difpos0<-10){
-            //uart_puts (uart0,"right");
             pwm_set_gpio_level(2, (uint16_t)fmax(0,(105+(difpos0/2))));
-            
             gpio_put(3,1);
             sleep_ms(1);
             pwm_set_gpio_level(2, 0);
             gpio_put(3,0);
         }
         else if (difpos0>10){
-            //uart_puts (uart0,"left"); 
             pwm_set_gpio_level(2, (uint16_t)fmin(100,((difpos0/2)-5)));
-                  
             gpio_put(3,0);
             sleep_ms(1);
             pwm_set_gpio_level(2, 0);
             gpio_put(3,0);
         }
-        
-        
         gpio_put(11,1);
-        
         adc_select_input(1);
         temadcinr1 = adc_read();
         curpos1 = curpos1+posdiff(adcinr1,temadcinr1);
         adcinr1 = temadcinr1;
         difpos1 =tarpos1-curpos1;
         if (difpos1<-10){
-            //uart_puts (uart0,"right");
             pwm_set_gpio_level(26, (uint16_t)fmax(0,(105+(difpos1/2))));
-            
             gpio_put(27,1);
-            sleep_ms(1);
+            sleep_ms(0.5);
             pwm_set_gpio_level(26, 0);
             gpio_put(27,0);
         }
         else if (difpos1>10){
-            //uart_puts (uart0,"left"); 
             pwm_set_gpio_level(26, (uint16_t)fmin(100,((difpos1/2)-5)));
-                  
             gpio_put(27,0);
-            sleep_ms(1);
+            sleep_ms(0.5;)
             pwm_set_gpio_level(26, 0);
             gpio_put(27,0);
         }
-        
-        
-        
         gpio_put(11,0);
         sleep_ms(10);
-        
     }
-
 }
 void main() {
+    INIT:
     gpio_init(11);
-    //gpio_init(16);
     gpio_init(17);
-    //gpio_init(2);
     gpio_init(3);
     gpio_init(13);
     gpio_init(18);
@@ -153,64 +123,23 @@ void main() {
     pwm_set_wrap(0, 100);
     pwm_set_chan_level(0, PWM_CHAN_A, 0);
     pwm_set_enabled(0, true);
-    //gpio_set_dir(16,1);
     gpio_set_dir(17,1);
-    //gpio_set_dir(2,1);
     gpio_set_function(2, 4);
     pwm_set_wrap(1, 100);
     pwm_set_chan_level(1, PWM_CHAN_A, 0);
     pwm_set_enabled(1, true);
     gpio_set_dir(3,1);
     gpio_set_dir(13,1);
-    //gpio_put(16,0);
     gpio_put(17,0);
-    //gpio_put(2,0);
     gpio_put(3,0);
     gpio_put(13,1);
     gpio_put(11,1);
     gpio_put(18,0);
-    //stdio_usb_init();	
-
-
-     
-    while(false){
-        gpio_put(11,1);
-        gpio_put(2,1);
-        gpio_put(3,0);
-        gpio_put(16,1);
-        gpio_put(17,0);
-
-        sleep_ms(1000);
-
-        gpio_put(11,0);
-        gpio_put(2,0);
-        gpio_put(3,1);
-        gpio_put(16,0);
-        gpio_put(17,1);
-        sleep_ms(1000);
-
-    }
-    
-    //uint8_t did[SBUF_LEN];
-    // Enable SPI 
     stdio_init_all();
-    
-    //spi_init(spi0, 1000 * 1000);
-    //spi_set_slave(spi0, true);
-    /*
-    gpio_set_function(20, 1);
-    gpio_set_function(21, 1);
-    gpio_set_function(22, 1);
-    gpio_set_function(23, 1);
-    */
-    // Make the SPI pins available to picotool
-    //bi_decl(bi_4pins_with_func(20, 23, 22, 21, 1));
     in_ok = gpio_get(19);
     while (in_ok==0){
         in_ok = gpio_get(19);
     }
-
-
     // init uart
     // Enable UART for comunicating
     uart_init (uart1,38400);
@@ -222,85 +151,17 @@ void main() {
             uart_read_blocking(uart1,ddid,2);
             break;
         }
-
     }
     uart_puts(uart1,"ok");
     gpio_put(18,1);
-    
-
-
-    
-    /*
-    while(false){
-        if(uart_is_enabled(uart0)){
-            adc_select_input(0);
-            fresult = adc_read();
-            fresult = fresult/4048;
-            fresult = sin(fresult * M_PI);
-            sprintf(int_str, "%g", fresult);
-            uart_puts (uart0,int_str);
-            uart_puts (uart0,"\n");
-            adc_select_input(1);
-            fresult = adc_read();
-            fresult = fresult/4048;
-            fresult = sin(fresult * M_PI);
-            sprintf(int_str, "%g", fresult);
-            uart_puts (uart0,int_str);
-            uart_puts (uart0,"\n");
-            sleep_ms(500);
-        }
-    }
-    */
-    
-    
-    
-    while (false){
-    in_buf0[0] = 0x0a;
-    in_buf0[1] = 0xc1;
-    in_buf0[2] = 0x2d;
-    cmaxi = (in_buf0[1] >> 7) & 0x1;
-    cmdir = (in_buf0[1] >> 6) & 0x1;
-    msdel = (in_buf0[1] & 0x3f) << 8;
-    msdel = msdel + in_buf0[2];
-    cmdel = msdel;
-    //sprintf (scmdel,"%d", cmdel);
-    //uart_puts (uart0,scmdel);
-    //uart_puts (uart0,"\nd:");
-    sleep_ms(500);
-    }
-    
     adc_select_input(0);
     adcinr0 = adc_read();
     adc_select_input(1);
     adcinr1 = adc_read();
-    if (false){
-        while(ddid=='0'){
-            if (spi_is_readable(spi0)){
-                spi_read_blocking(spi0,0,sin_buf0,1);
-                ddid=sin_buf0[0];
-                
-             
-            }
-            if (uart_is_enabled(uart0)){
-                //uart_puts (uart0,"\nddid:");
-                //uart_puts (uart0,ddid);
-            }
-
-        }
-    }
-    
     gpio_put(11,0);
     gpio_put(13,0);
-    //spi_write_blocking(spi0,ddid,BUF_LEN);
     multicore_launch_core1(core1);
-    while(true){
-        //to do:    ok close loop control*2(X+Y) pid / inching / compairator
-        //          indexing protocol   [id][/|\(x/y)][-|+(dir)][int(amount)][~(end)]
-        //          ok calibrate 0   
-        //          ok wrapping angle  using expected rotation dir/ full rot counting/ sine - cosine filter
-        //          ok analogue in    using adc
-        //          soft e stop
-        //          
+    while(true){ 
         if(true){
             if (uart_is_readable(uart1)){
                 uart_read_blocking(uart1,in_buf0,BUF_LEN);
@@ -319,7 +180,6 @@ void main() {
                             tarpos0 = ((int)msdel)* -1 ;
                         }
                     }
-                    
                     else{
                         if (cmdir){
                             tarpos1 = (int)msdel;
@@ -328,21 +188,15 @@ void main() {
                             tarpos1 = ((int)msdel)* -1 ;
                         }
                     }
-                    /*
-                    cmdel = msdel;
-                    sprintf (scmdel,"%d", cmdel);
-                    uart_puts (uart0,scmdel);
-                    uart_puts (uart0,"\nd:");
-                    */
+                }
+                if (in_bufid == 0){
+                    goto INIT;
                 }
             }
-
         }
-        
         if (false){
             if (spi_is_readable(spi0)){
                 spi_read_blocking(spi0,0x00,in_buf0,BUF_LEN);
-                //chin_buf0 = in_buf0;
                 if (in_buf0[0] == ddid){
                     cmaxi = (in_buf0[1] >> 7) & 0x1;
                     cmdir = (in_buf0[1] >> 6) & 0x1;
@@ -356,7 +210,6 @@ void main() {
                             tarpos0 = ((int)msdel)* -1 ;
                         }
                     }
-                    
                     else{
                         if (cmdir){
                             tarpos1 = (int)msdel;
@@ -365,69 +218,8 @@ void main() {
                             tarpos1 = ((int)msdel)* -1 ;
                         }
                     }
-                    /*
-                    cmdel = msdel;
-                    sprintf (scmdel,"%d", cmdel);
-                    uart_puts (uart0,scmdel);
-                    uart_puts (uart0,"\nd:");
-                    */
                 }
             }
         }
-        
-        
-        
-        
-        if(false){
-            if (uart_is_enabled(uart0)){
-                if (uart_is_writable(uart0)){
-                    sprintf (sresult,"ad0:%d  ad1:%d  ta0:%d  ta1:%d  cp0:%d  cp1:%d  tp0:%d  tp1:%d \n",adcinr0 , adcinr1 , temadcinr0 , temadcinr1 , curpos0 ,curpos1,tarpos0,tarpos1);
-                    uart_puts (uart0,sresult);
-                    sprintf (sresult,"0b%d\n",in_buf0);
-                    uart_puts (uart0,sresult);
-                }
-                /*
-                if (uart_is_readable(uart0)){
-                    uin = uart_getc (uart0);
-                    while('\n' != uin){
-                        suin = suin*10 + ((int)uin);
-                        uart_puts (uart0,"read\n");
-                        uin = uart_getc (uart0);
-                    }
-                }
-                */
-                //uart_puts (uart0,"\n");
-                //uart_puts (uart0,ddid);
-                //gpio_put(11,1);
-                //sleep_ms(3);
-                //gpio_put(11,0);
-            }
-        }
-        
-        //printf ("ad0:%d  ad1:%d  ta0:%d  ta1:%d  cp0:%d  cp1:%d  tp0:%d  tp1:%d \n",adcinr0 , adcinr1 , temadcinr0 , temadcinr1 , curpos0 ,curpos1,tarpos0,tarpos1);
-        //printf ("0b%d\n",in_buf0);
-        //tarpos0 = suin;
-        /**
-        gpio_put(11,1);
-        sleep_ms(3);
-        gpio_put(11,0);
-        **/
-        //sleep_ms(10);
-        /*
-        tarpos0 = 5094;
-        sleep_ms(1500);
-        tarpos0 = -5094;
-        sleep_ms(1500);
-        */
-		/*
-        //sprintf (sresult,"0b%d\n",in_buf0);
-        //uart_puts (uart0,"hi\n");
-        //sleep_ms(100);
-        //sprintf (sresult,"ad0:%d  ad1:%d  ta0:%d  ta1:%d  cp0:%d  cp1:%d  tp0:%d  tp1:%d \n",adcinr0 , adcinr1 , temadcinr0 , temadcinr1 , curpos0 ,curpos1,tarpos0,tarpos1);
-        //sprintf(sresult,"sjfiewafji");
-        uart_puts (uart0, sresult );
-        uart_puts (uart0, "\n" );
-        sleep_ms(1000);
-		*/
     }
 }
